2f30f1f4329575506f78e67400d5ae1f753f76cd
Jakub Kisielewski <kisielewskij66@gmail.com>
2025-10-25 16:51:10 +0100

    added product info

--- backend/index.js
+++ backend/index.js
@@ -42,9 +42,6 @@ app.get("/", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 // end of test
 
 app.use(cookieParser());
