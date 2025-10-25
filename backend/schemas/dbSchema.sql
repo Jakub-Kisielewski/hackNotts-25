7f277324be90f1920c8c4e3228422f380e76abaf
night0721 <night@night0721.xyz>
2025-10-25 19:34:59 +0100

    backend auth and db integration

--- backend/schemas/dbSchema.sql
+++ backend/schemas/dbSchema.sql
@@ -52,3 +52,15 @@ CREATE TABLE receipts (
     created_at TIMESTAMP DEFAULT NOW()
 );
 
+
+CREATE TABLE sessions (
+  sid varchar NOT NULL COLLATE "default",
+  sess json NOT NULL,
+  expire timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+
+ALTER TABLE sessions ADD CONSTRAINT "session_pkey" PRIMARY KEY ("sid");
+
+CREATE INDEX "IDX_session_expire" ON sessions ("expire");
+
